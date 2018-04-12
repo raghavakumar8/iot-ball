8a5781ff485608cb5f40f64fbf6585d8d33c954b
Raghava Kumar <raghava.kumar8@gmail.com>
2018-04-12 15:08:49 -0500

    Fix websocket server

--- web/app.py
+++ web/app.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # Reference: http://banjolanddesign.com/flask-google-charts.html
 
+import sys
 from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+from flask_sockets import Sockets
 
 __author__ = "raghava@maidbot.co (Raghava Kumar)"
 
 app = Flask(__name__, template_folder='templates')
-socketio = SocketIO(app)
+sockets = Sockets(app)
 
 @app.route('/')
 def load_chart():
@@ -15,18 +16,19 @@ def load_chart():
     data = {'row_data': data}
     return render_template('chart.html', data=data)
 
-@socketio.on('message', namespace='/data')
-def test_message(message):
-    send(message)
-
-@socketio.on('connect', namespace='/data')
-def test_connect():
-    print 'Client connected'
-
-@socketio.on('disconnect', namespace='/data')
-def test_disconnect():
-    print 'Client disconnected'
+@sockets.route('/echo')
+def echo_socket(ws):
+    print "Client connected to: /echo"
+    sys.stdout.flush()
+    while not ws.closed:
+        message = ws.receive()
+        ws.send(message)
+    print "Client disconnected"
+    sys.stdout.flush()
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=80)
+    from gevent import pywsgi
+    from geventwebsocket.handler import WebSocketHandler
+    server = pywsgi.WSGIServer(('0.0.0.0', 80), app, handler_class=WebSocketHandler)
+    server.serve_forever()
     
